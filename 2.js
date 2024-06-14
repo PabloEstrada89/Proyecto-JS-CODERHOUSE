6fe450dccb318705e50e2ec91a67375c4975a012
Pablo Estrada <pablo.estrada.89@gmail.com>
2024-06-14 07:30:13 -0300

    PrimeraEntrega

---
+++ 2.js
@@ -0,0 +1,18 @@
+// let PLA = 15000
+// let ABS = 20000
+// let PETG = 35000
+
+
+const filamentoPLA = {
+    filamento: "PLA",
+    precio: 15000
+}
+const filamentoABS = {
+    filamento: "ABS",
+    precio: 15000
+}
+const filamentoPETG = {
+    filamento: "PETG",
+    precio: 15000
+}
+const filamentos[filamento1, filamento2, filamento3]
